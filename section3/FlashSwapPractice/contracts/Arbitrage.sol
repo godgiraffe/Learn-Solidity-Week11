cae3d75a15178d4ea07b393f4f2c613ece4d2b9a
roy_chen <roy_chen@gss.com.tw>
2023-05-28 16:27:46 +0800

    2 個 pool 之間的套利

--- section3/FlashSwapPractice/contracts/Arbitrage.sol
+++ section3/FlashSwapPractice/contracts/Arbitrage.sol
@@ -5,18 +5,27 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IUniswapV2Pair } from "v2-core/interfaces/IUniswapV2Pair.sol";
 import { IUniswapV2Callee } from "v2-core/interfaces/IUniswapV2Callee.sol";
+import { IWETH } from "v2-periphery/interfaces/IWETH.sol";
 
 // This is a pracitce contract for flash swap arbitrage
 contract Arbitrage is IUniswapV2Callee, Ownable {
+    // struct CallbackData {
+    //     address borrowPool;
+    //     address targetSwapPool;
+    //     address borrowToken;
+    //     address debtToken;
+    //     uint256 borrowAmount;
+    //     uint256 debtAmount;
+    //     uint256 debtAmountOut;
+    // }
     struct CallbackData {
-        address borrowPool;
-        address targetSwapPool;
-        address borrowToken;
-        address debtToken;
-        uint256 borrowAmount;
-        uint256 debtAmount;
-        uint256 debtAmountOut;
+        uint borrowTokenAmount;
+        uint repayToLowerPoolAmount;
+        address priceLowerPool;
+        address priceHigherPool;
     }
+    uint8 constant USDC_DECIMAL = 6;
+    uint8 constant WETH_DECIMAL = 18;
 
     //
     // EXTERNAL NON-VIEW ONLY OWNER
@@ -39,10 +48,22 @@ contract Arbitrage is IUniswapV2Callee, Ownable {
         require(sender == address(this), "Sender must be this contract");
         require(amount0 > 0 || amount1 > 0, "amount0 or amount1 must be greater than 0");
 
-        // 3. decode callback data
-        // 4. swap WETH to USDC
-        // 5. repay USDC to lower price pool
+        (uint borrowTokenAmount, uint repayToLowerPoolAmount, address priceLowerPool, address priceHigherPool) = abi
+            .decode(data, (uint, uint, address, address));
+        address wethAddr = IUniswapV2Pair(priceLowerPool).token0();
+        address usdcAddr = IUniswapV2Pair(priceLowerPool).token1();
+
+        // 計算借來的 ether 能換到多少 usdc
+        uint getUsdcAmount = _getAmountOut(borrowTokenAmount, 50 ether, 6_000 * 10 ** USDC_DECIMAL); // 543966536
+
+        // 因為是直接 call swap，所以要主動打 weth 進 pool
+        IERC20(wethAddr).transfer(address(priceHigherPool), borrowTokenAmount);
+        IUniswapV2Pair(priceHigherPool).swap(0, getUsdcAmount, sender, "");
+
+        // 把需要還的 usdc 還回去
+        IERC20(usdcAddr).transfer(address(priceLowerPool), repayToLowerPoolAmount);
     }
+
 
     // Method 1 is
     //  - borrow WETH from lower price pool
@@ -54,11 +75,16 @@ contract Arbitrage is IUniswapV2Callee, Ownable {
     //  - repay WETH to higher pool
     // for testing convenient, we implement the method 1 here
     function arbitrage(address priceLowerPool, address priceHigherPool, uint256 borrowETH) external {
-        // 1. finish callbackData
-        // 2. flash swap (borrow WETH from lower price pool)
+        // 計算借 borrowETH 的話，要還多少 token 回去
+        uint256 repayToLowerPoolAmount = _getAmountIn(borrowETH, 4_000 * 10 ** USDC_DECIMAL, 50 ether);
 
-        // Uncomment next line when you do the homework
-        // IUniswapV2Pair(priceLowerPool).swap(borrowETH, 0, address(this), abi.encode(callbackData));
+        CallbackData memory data;
+        data.borrowTokenAmount = borrowETH;
+        data.repayToLowerPoolAmount = repayToLowerPoolAmount;
+        data.priceLowerPool = priceLowerPool;
+        data.priceHigherPool = priceHigherPool;
+
+        IUniswapV2Pair(priceLowerPool).swap(borrowETH, 0, address(this), abi.encode(data));
     }
 
     //
